3567cbfbeeeb6de554f31473f90295854a86285f
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-04-17 15:44:08 -0400

    Initial drop

---
+++ HISTORY.md
@@ -0,0 +1,7 @@
+Express State Change History
+============================
+
+0.0.1 (2013-04-17)
+------------------
+
+* Initial release.
---
+++ examples/basic/server.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    exphbs  = require('express3-handlebars'),
+    expose  = require('../../'),
+
+    app = express();
+
+app.engine('hbs', exphbs());
+app.set('view engine', 'hbs');
+
+app.expose({
+    flickr_api: 'asdf'
+});
+
+app.get('/', function (req, res, next) {
+    res.expose({
+        user      : {id: '1234'},
+        flickr_api: 'fdsa'
+    });
+
+    res.expose('Eric', 'user.name');
+
+    res.render('index');
+});
+
+app.get('/foo', function (req, res, next) {
+    res.expose({
+        user: {id: '1234'}
+    });
+
+    res.render('index');
+});
+
+app.expose('MY APP', 'title', 'app');
+
+app.listen(3000);
