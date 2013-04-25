2e67679e292a85f4cde4e11b4c8e5bf2720e2c1d
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-04-25 17:32:51 -0400

    Move lib/state to index

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-module.exports = require('./lib/state');
+'use strict';
+
+var express = require('express'),
+    Exposed = require('./exposed'),
+
+    appProto = express.application,
+    resProto = express.response;
+
+exports.local = 'state';
+
+// Modifies Express' `application` and `response` prototypes by adding the
+// `expose()` method.
+resProto.expose = appProto.expose = function (obj, namespace, local) {
+    local || (local = exports.local);
+
+    var locals    = this.locals,
+        appLocals = this.app && this.app.locals,
+        exposed   = locals[local];
+
+    if(!(exposed instanceof Exposed)) {
+        // Creates a new `Exposed` instance, and links its prototype to the
+        // corresponding app exposed object, if one exists.
+        exposed = locals[local] = Exposed.create(appLocals && appLocals[local]);
+    }
+
+    if (namespace) {
+        exposed.add(namespace, obj);
+    } else {
+        Object.keys(obj).forEach(function (key) {
+            exposed.add(key, obj[key]);
+        });
+    }
+};
