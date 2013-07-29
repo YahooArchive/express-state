e64fa91a6237358eba441395c7a7da8b5e649385
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-07-29 15:38:36 -0400

    Add tests for `extend()` and `augment()`

--- test/multiple.js
+++ test/multiple.js
@@ -5,6 +5,13 @@ var expect  = require('chai').expect,
     mockery = require('mockery');
 
 describe('multiple', function () {
+    function createExpressMock() {
+        return {
+            application: {},
+            response   : {}
+        };
+    }
+
     beforeEach(function () {
         mockery.enable({
             useCleanCache     : true,
@@ -12,10 +19,7 @@ describe('multiple', function () {
             warnOnUnregistered: false
         });
 
-        mockery.registerMock('express', {
-            application: {},
-            response   : {}
-        });
+        mockery.registerMock('express', createExpressMock());
     });
 
     afterEach(function () {
@@ -32,5 +36,68 @@ describe('multiple', function () {
 
         expect(express.application.expose).to.equal(expose);
         expect(express.response.expose).to.equal(expose);
+    });
+
+    describe('.extend( express )', function () {
+        it('should add `expose()`', function () {
+            var express = require('express'),
+                state   = require('../');
+
+            mockery.registerMock('express', createExpressMock());
+            express = require('express');
+
+            expect(express.application.expose).to.be.undefined;
+            expect(express.response.expose).to.be.undefined;
+
+            state.extend(express);
+
+            expect(express.application).itself.to.respondTo('expose');
+            expect(express.response).itself.to.respondTo('expose');
+        });
+
+        it('should not override `expose()` if it exists', function () {
+            var expose = function () {},
+                express, state;
+
+            express = require('express');
+            state   = require('../');
+
+            express.application.expose = express.response.expose = expose;
+            state.extend(express);
+
+            expect(express.application.expose).to.equal(expose);
+            expect(express.response.expose).to.equal(expose);
+        });
+    });
+
+    describe('.augment( app )', function () {
+        it('should add `expose()`', function () {
+            var express = require('express'),
+                state   = require('../'),
+                app     = {response: {}};
+
+            state.augment(app);
+
+            expect(app).itself.to.respondTo('expose');
+            expect(app.response).itself.to.respondTo('expose');
+        });
+
+        it('should not override `expose()` if it exists', function () {
+            var expose = function () {},
+                express, state, app;
+
+            express = require('express');
+            state   = require('../');
+
+            app = {
+                expose  : expose,
+                response: {expose: expose}
+            };
+
+            state.augment(app);
+
+            expect(app.expose).to.equal(expose);
+            expect(app.response.expose).to.equal(expose);
+        });
     });
 });
