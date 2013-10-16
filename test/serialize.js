1936bafd0ee3b611eb351b389707ec4684762fd1
Eric Ferraiuolo <eferraiuolo@gmail.com>
2013-10-16 12:38:22 -0400

    Cache the unsafe-html-chars regex and enhance XSS unit test

--- test/serialize.js
+++ test/serialize.js
@@ -160,6 +160,7 @@ describe('serialize', function () {
     describe('XSS', function () {
         it('should encode unsafe HTML chars to Unicode', function () {
             expect(serialize('</script>')).to.equal('"\\u003C\\u002Fscript\\u003E"');
+            expect(JSON.parse(serialize('</script>'))).to.equal('</script>');
             expect(eval(serialize('</script>'))).to.equal('</script>');
         });
     });
