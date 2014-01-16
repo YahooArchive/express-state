35c1e0447df81d79030defcaeb1b81c2b5715ee1
Eric Ferraiuolo <eferraiuolo@gmail.com>
2014-01-16 03:38:09 -0500

    Improve serialization benchmark tests and add PhotosNearMe fixture

--- test/benchmark/serialize.js
+++ test/benchmark/serialize.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var Benchmark = require('benchmark'),
-    Exposed   = require('../../lib/exposed'),
-    serialize = require('../../lib/serialize'),
-    ytFixture = require('../fixtures/yahoo-tech');
+var Benchmark  = require('benchmark'),
+    Exposed    = require('../../lib/exposed'),
+    serialize  = require('../../lib/serialize'),
+    ytFixture  = require('../fixtures/yahoo-tech'),
+    pnmFixture = require('../fixtures/photosnearme');
 
 var suiteConfig = {
     onStart: function (e) {
@@ -19,24 +20,38 @@ var suiteConfig = {
     }
 };
 
+function expose(exposed, fixture, namespaces, options) {
+    namespaces.forEach(function (namespace) {
+        var parts = namespace.split('.'),
+            part  = parts.shift(),
+            value = fixture;
+
+        while (part) {
+            value = value[part];
+            part  = parts.shift();
+        }
+
+        exposed.add(namespace, value, options);
+    });
+
+    return exposed;
+}
+
+// -- simpleOjb ----------------------------------------------------------------
+
 var simpleObj = {
     foo: 'foo',
     bar: false,
-    num: 100
+    num: 100,
+    arr: [1, 2, 3, 4],
+    obj: {baz: 'baz'}
 };
 
-var simpleExposed = Exposed.create();
-simpleExposed.add('window', simpleObj);
+var simple = Exposed.create();
+simple.add('simple', simpleObj);
 
-var appExposed = Exposed.create();
-appExposed.add('app.yui', ytFixture.app.yui);
-appExposed.add('YUI_config', ytFixture.YUI_config);
-appExposed.add('YUI_config.seed', ytFixture.YUI_config.seed);
-
-var resExposed = Exposed.create(appExposed);
-Object.keys(ytFixture.App.Cache).forEach(function (key) {
-    resExposed.add('App.Cache.' + key, ytFixture.App.Cache[key]);
-});
+var simpleCached = Exposed.create();
+simpleCached.add('simple', simpleObj, {cache: true});
 
 new Benchmark.Suite('simpleObj', suiteConfig)
     .add('JSON.stringify( simpleObj )', function () {
@@ -45,31 +60,149 @@ new Benchmark.Suite('simpleObj', suiteConfig)
     .add('serialize( simpleObj )', function () {
         serialize(simpleObj);
     })
-    .add('simpleExposed.toString()', function () {
-        simpleExposed.toString();
+    .add('simple.toString()', function () {
+        simple.toString();
+    })
+    .add('simpleCached.toString()', function () {
+        simpleCached.toString();
     })
     .run();
 
-new Benchmark.Suite('YUI_config', suiteConfig)
-    .add('JSON.stringify( YUI_config )', function () {
-        JSON.stringify(ytFixture.YUI_config);
+// -- PNM ----------------------------------------------------------------------
+
+var pnmApp = expose(
+    Exposed.create(),
+    pnmFixture,
+    [
+        'PNM.CACHE',
+        'PNM.FLICKR',
+        'PNM.ROUTES',
+        'app.yui',
+        'YUI_config',
+        'YUI_config.seed'
+    ]
+);
+
+var pnmAppCached = expose(
+    Exposed.create(),
+    pnmFixture,
+    [
+        'PNM.CACHE',
+        'PNM.FLICKR',
+        'PNM.ROUTES',
+        'app.yui',
+        'YUI_config',
+        'YUI_config.seed'
+    ], {cache: true}
+);
+
+var pnmRes = expose(
+    Exposed.create(pnmApp),
+    pnmFixture,
+    [
+        'PNM.DATA',
+        'PNM.DATA.place',
+        'PNM.DATA.photos',
+        'PNM.VIEW'
+    ]
+);
+
+var pnmResAppCached = expose(
+    Exposed.create(pnmAppCached),
+    pnmFixture,
+    [
+        'PNM.DATA',
+        'PNM.DATA.place',
+        'PNM.DATA.photos',
+        'PNM.VIEW'
+    ]
+);
+
+var pnmResCached = expose(
+    Exposed.create(pnmAppCached),
+    pnmFixture,
+    [
+        'PNM.DATA',
+        'PNM.DATA.place',
+        'PNM.DATA.photos',
+        'PNM.VIEW',
+    ], {cache: true}
+);
+
+new Benchmark.Suite('PNM', suiteConfig)
+    .add('pnmApp.toString()', function () {
+        pnmApp.toString();
     })
-    .add('serialize( YUI_config )', function () {
-        serialize(ytFixture.YUI_config);
+    .add('pnmAppCached.toString()', function () {
+        pnmAppCached.toString();
     })
-    .add('appExposed.toString()', function () {
-        appExposed.toString();
+    .add('pnmRes.toString()', function () {
+        pnmRes.toString();
+    })
+    .add('pnmResAppCached.toString()', function () {
+        pnmResAppCached.toString();
+    })
+    .add('pnmResCached.toString()', function () {
+        pnmResCached.toString();
     })
     .run();
 
-new Benchmark.Suite('App.Cache', suiteConfig)
-    .add('JSON.stringify( App.Cache )', function () {
-        JSON.stringify(ytFixture.App.Cache);
+// -- YT -----------------------------------------------------------------------
+
+var ytApp = expose(
+    Exposed.create(),
+    ytFixture,
+    [
+        'app.yui',
+        'YUI_config',
+        'YUI_config.seed'
+    ]
+);
+
+var ytAppCached = expose(
+    Exposed.create(),
+    ytFixture,
+    [
+        'app.yui',
+        'YUI_config',
+        'YUI_config.seed'
+    ], {cache: true}
+);
+
+var ytRes = expose(
+    Exposed.create(ytApp),
+    ytFixture,
+    Object.keys(ytFixture.App.Cache)
+);
+
+
+var ytResAppCached = expose(
+    Exposed.create(ytAppCached),
+    ytFixture,
+    Object.keys(ytFixture.App.Cache)
+);
+
+var ytResCached = expose(
+    Exposed.create(ytAppCached),
+    ytFixture,
+    Object.keys(ytFixture.App.Cache),
+    {cache: true}
+);
+
+new Benchmark.Suite('YT', suiteConfig)
+    .add('ytApp.toString()', function () {
+        ytApp.toString();
     })
-    .add('serialize( App.Cache )', function () {
-        serialize(ytFixture.App.Cache);
+    .add('ytAppCached.toString()', function () {
+        ytAppCached.toString();
     })
-    .add('resExposed.toString()', function () {
-        resExposed.toString();
+    .add('ytRes.toString()', function () {
+        ytRes.toString();
+    })
+    .add('ytResAppCached.toString()', function () {
+        ytResAppCached.toString();
+    })
+    .add('ytResCached.toString()', function () {
+        ytResCached.toString();
     })
     .run();
